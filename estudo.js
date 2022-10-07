e602c5317cc763f5e497a834d1e3314d12a053e7
MAIKEWILLIAM <110178731+Oligarc69@users.noreply.github.com>
2022-10-07 10:01:59 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,31 +1,50 @@
-let tabuada = 5;
+let tabuada = 6;
 
 function escreva(){
     document.write("Tabuada do " + tabuada + "<br>")
-    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
-    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
-    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
-    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
-    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
-    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
-    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
-    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
-    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
-    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
+    document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada+" x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada+" x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada+" x 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada+" x 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada+" x 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada+" x 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada+" x 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada+" x 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada+" x 10 = "+(tabuada*10)+"<br>");
 }
 
-let lista = ["Maike","jorge","spessato","raquel","waldete","jesus"];
+let lista = ["Terra","Marte","Saturno","Jupiter","Vênus","Mercurio"];
 
-function mostra (){
-      document.write(lista.length + "<br>");
-      for(let i = 0; i < 3; i++){
-
-         document.write(lista[i] + "<br>");
-      }
+function mostraLista(){
+    document.write("Tamanho da lista: " + lista.length + "<br>");
+    for(let i = 0; i < lista.length; i++){
+        document.write("Planela " + lista[i] + "<br>");
     }
-    function mostratabuada(){
-        for(let i = 1; i <= 10; i++){
-            document.write("o valor do i " + i + "<br>");
+}
+
+function repete(){
+    for(let i = 1; i <= 10; i++){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j <=10; j++){
+            document.write(i+" x " + j + " = "+(i*j)+"<br>");
         }
     }
+}
+function total(){
+    let v = document.getElementById("valor").ariaValueMax;
+    let j = document.getElementById("juros").ariaValueMax;
+    let t = document.getElementById("meses").ariaValueMax;
+    let r = 0;
+    for(let i =1; i <=t; i++){
+         r = v * (1 + (j/100));
+         document.write("Mês " + i + " - valor:" + r + "<br>"); 
+         v = r;  
+
+    }
+    
+    document.write("resultado: " + r);
+}
+
+
 
